748b1934fbc5bc3dd978f8906a11f5a30ce428e7
root <root@ip-10-34-10-76.ec2.internal>
2014-04-14 20:49:23 +0000

    oauth

--- app/controllers/OAuthController.scala
+++ app/controllers/OAuthController.scala
@@ -1,12 +1,13 @@
 package stack_notify.controllers
 
+import stack_notify.models.{User, UserModel}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WS
 import play.api.Play.current
 import play.api.mvc._
 
 
-object OAuthController extends Controller {
+object OAuthController extends StackNotifyController {
 
 
 	//val Host = "localhost:9000"
@@ -16,7 +17,7 @@ object OAuthController extends Controller {
 	val ClientSecret = "OByZgeorlcL4k7NfIfpVAA(("
 	val Key = "uzuwlVXnOAAwH*PM0goEPw(("
 	val ClientId = "2836"
-	val CallbackUrl = "http://" + Host + "/oauth/callback"
+	val CallbackUrl = "http://" + Host + "/oauth/"
 
 	val PostRequest = "https://stackexchange.com/oauth/access_token"
 
@@ -24,24 +25,36 @@ object OAuthController extends Controller {
 		StackExchange + "?client_id=" + ClientId + "&redirect_uri=" + CallbackUrl
 	}
 	
-	def getPostParams(code: String): Map[String, Seq[String]] = {
+	def getPostParams(code: String, googleId: String): Map[String, Seq[String]] = {
 		Map(
 			"client_id" -> Seq(ClientId),
 			"client_secret" -> Seq(ClientSecret),
 			"code" -> Seq(code),
-			"redirect_uri" -> Seq(CallbackUrl)
+			"redirect_uri" -> Seq(CallbackUrl + googleId + "/callback")
 		)
 	}
 
 	def callback = Action { implicit request => 
 		Async {
-			println("request " + request)
-
+			
 			val code = request.getQueryString("code").get
-
-			WS.url(PostRequest).post(getPostParams(code)).map { response =>
-				println("response.body " + response.body)
-				Ok(response.body)
+			val idRegex = ".*/oauth/(.+)/.*".r
+			val googleId = request.path match {
+				case idRegex(group) => group
+			}
+			WS.url(PostRequest).post(getPostParams(code, googleId)).map { response =>
+				val regex = "access_token=(.+)&.*".r
+				val access = response.body match {
+					case regex(group) => group
+				}
+				val user = UserModel.findByGoogleId(googleId)
+				if (user.isDefined) {
+					UserModel.update(user.get.copy(accessToken = Some(access)))
+					Ok("Successfully connected. It is safe to close this window.")
+				}
+				else {
+					failure("Unable to find user")
+				}
 			}
 		}
 	}
